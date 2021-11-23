f8369d23b6a94b63c2d3c5a9ca4258485fd7f60a
liangxuewei <Itachi_invincible@163.com>
2021-11-23 17:36:40 +0800

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_itachi/util/handle_error_utils.dart';
 import 'package:sp_util/sp_util.dart';
 
-Future<void> main async {
+Future<void> main() async {
 
-/// sp初始化
+  /// sp初始化
   await SpUtil.getInstance();
 
-/// 1.22 预览功能: 在输入频率与显示刷新率不匹配情况下提供平滑的滚动效果
-// GestureBinding.instance?.resamplingEnabled = true;
-/// 异常处理
+  /// 1.22 预览功能: 在输入频率与显示刷新率不匹配情况下提供平滑的滚动效果
+  // GestureBinding.instance?.resamplingEnabled = true;
+  /// 异常处理
   handleError(() => runApp(MyApp()));
 
-
+  /// 隐藏状态栏。为启动页、引导页设置。完成后修改回显示状态栏。
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]);
 }
+
 
